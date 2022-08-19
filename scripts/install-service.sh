249c2251fde75ecc9fc560b9e406edaf34c300df
Radical <benjaminjohnsen.bgj@gmail.com>
2022-08-20 00:51:02 +0200

    Add scripts to source

---
+++ scripts/install-service.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+if whoami = "root"
+then
+    echo "This script cannot be ran as root"
+    exit 1
+fi
+
+sed "s/CHANGEME/$USER/g" steam-presence.service > steam-presence.service.tmp
+
+# Find .env file
+if [ -f .env ];
+then
+    echo "Found .env file"
+else
+    echo "No .env file found"
+    echo "Please create one or run the 'steam-presence-on-discord' executable to make one automatically"
+    exit 1
+fi
+
+
+echo "Installing systemd service"
+cp steam-presence.service.tmp ~/.config/systemd/user/steam-presence.service
+rm steam-presence.service.tmp
+
+echo "Moving files to ~/.config/steam-presence"
+mkdir -p ~/.config/steam-presence
+cp steam-presence-on-discord ~/.config/steam-presence/steam-presence-on-discord
+cp .env ~/.config/steam-presence/.env
+
+echo "Enable and start the service with 'systemctl --user enable steam-presence.service && systemctl --user start steam-presence.service'"
+exit 0
